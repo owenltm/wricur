f125d22050d7f419f8667a97c1b1b974e67433b1
user <user@users-macbook-pro.local>
2021-12-06 13:07:18 +0800

    Initial Commit

---
+++ wricur/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wricur
+//
+//  Created by user on 06/12/21.
+//  Copyright © 2021 ltm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
